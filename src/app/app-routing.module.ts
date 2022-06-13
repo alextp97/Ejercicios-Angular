ea5f83adf9e776c147ec59cc2c3e01e359d08cbe
Alejandro Torres <alejandro.torres@bosonit.com>
2022-06-13 09:34:43 +0200

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentCommunicationComponent } from './components/component-communication/component-communication.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { DisplayHideComponent } from './components/display-hide/display-hide.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'displayHide',
+    component: DisplayHideComponent
+  },
+  {
+    path: 'componentCommunication',
+    component: ComponentCommunicationComponent
+  },
+  {
+    path: 'crud',
+    component: CrudComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'displayHide'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayHideComponent } from './components/display-hide/display-hide.component';
+import { ComponentCommunicationComponent } from './components/component-communication/component-communication.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayHideComponent,
+    ComponentCommunicationComponent,
+    CrudComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
