197b4e524b9338425d15f3ad79a04ce7403500c5
Alejandro Torres <alejandro.torres@bosonit.com>
2022-06-13 10:33:53 +0200

    Terminada la funcionalidad del primer ejercicio

--- src/app/components/display-hide/display-hide.component.ts
+++ src/app/components/display-hide/display-hide.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-hide.component.scss']
 })
 export class DisplayHideComponent implements OnInit {
+  
+  show: boolean = false; //La variable show la inicializo en false para que no muestre nada al principio
+  showText: string = 'Mostrar'; //Creo una variable que le asigne al botón un texto u otro dependiendo de si está oculto o activo
+
+
+  //Este método se encarga de cambiar el valor del botón
+  showHide() {
+    
+    if (this.show) {
+      this.show = false;
+      this.showText = 'Mostrar'
+    }
+    else {
+      this.show = true;
+      this.showText = 'Ocultar'
+    }
+  }
+
 
   constructor() { }
 
